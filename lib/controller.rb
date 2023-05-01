5805b5c6ca75817a6cdfdf045b5b41d9ea1276da
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-05-01 20:55:02 +0200

    modifs 2

--- lib/controller.rb
+++ lib/controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < Sinatra::Base
         Gossip.new(params["gossip_author"], params["gossip_content"]).save
         redirect '/'
     end
+
+    get '/gossips/:id/' do
+        erb :show, locals: {gossips: Gossip.find(params['id']), id: params['id']}
+    end
 end
